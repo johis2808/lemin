15e9e67e846b9779b746345457c5d36ca7d7600e
Thibault BERRIDO <thberrid@e1r1p12.42.fr>
2019-09-15 07:38:26 +0200

    PB normed'

--- path_back.c
+++ path_back.c
@@ -12,58 +12,7 @@
 
 #include "lem_in.h"
 
-static int	is_levelnull(int this_level, int graph_level)
-{
-	(void)graph_level;
-	if (!this_level)
-		return (1);
-	return (0);
-}
-
-static int	is_levelinf(int this_level, int graph_level)
-{
-	if (this_level < graph_level)
-		return (0);
-	return (1);
-}
-
-static int	no_twin(t_list *q, t_data *this)
-{
-	t_list	*twin;
-
-	twin = q;
-	while (twin)
-	{
-		if ((t_data *)twin->content == this)
-			return (0);
-		twin = twin->next;
-	}
-	return (1);
-}
-
-int			add_newq(t_list **q, t_list *list, int (*fn)(int, int), int level)
-{
-	t_data	*this;
-	t_list	*new_lst;
-	t_list	*tmp;
-
-	tmp = list;
-	while (tmp)
-	{
-		this = (t_data *)tmp->content;
-		if (fn(this->level, level) == 1 && no_twin(*q, this))
-		{
-			if (!(new_lst = ft_memalloc(sizeof(t_list))))
-				return (0);
-			new_lst->content = this;
-			ft_lstadd(q, new_lst);
-		}
-		tmp = tmp->next;
-	}
-	return (1);
-}
-
-int			bfs_level(t_list *q, int level)
+int				bfs_level(t_list *q, int level)
 {
 	t_list	*new_q;
 
@@ -80,17 +29,7 @@ int			bfs_level(t_list *q, int level)
 	return (0);
 }
 
-t_nodes		*make_path(t_nodes **head, t_data *new)
-{
-	if (!*head)
-		if (!(*head = ft_memalloc(sizeof(t_nodes))))
-			return (NULL);
-	if ((add_node(*head, new->name, new->role) < 0))
-		return (NULL);
-	return (*head);
-}
-
-t_list		*find_children(t_list *q, t_data *target)
+t_list			find_children(t_list *q, t_data *target)
 {
 	t_list *children;
 
@@ -113,7 +52,17 @@ t_list		*find_children(t_list *q, t_data *target)
 	return (q);
 }
 
-t_list		*add_room(t_list *q, t_list *new_room, t_nodes **path)
+t_nodes			*make_path(t_nodes **head, t_data *new)
+{
+	if (!*head)
+		if (!(*head = ft_memalloc(sizeof(t_nodes))))
+			return (NULL);
+	if ((add_node(*head, new->name, new->role) < 0))
+		return (NULL);
+	return (*head);
+}
+
+static t_list	*add_room(t_list *q, t_list *new_room, t_nodes **path)
 {
 	t_data	*tmp;
 
@@ -128,7 +77,7 @@ t_list		*add_room(t_list *q, t_list *new_room, t_nodes **path)
 	return (q);
 }
 
-t_list		*bfs_path(t_list *q, int level, t_nodes **path)
+t_list			*bfs_path(t_list *q, int level, t_nodes **path)
 {
 	t_list	*retrn;
 	t_list	*new_q;
