c9f7f0c17420105ab306ab9a5f707ab5a55e830e
Thibault BERRIDO <thberrid@e1r1p12.42.fr>
2019-09-15 07:32:38 +0200

    lst utils normed

--- lst_util.c
+++ lst_util.c
@@ -78,33 +78,3 @@ void		clear_tubes(t_nodes *nodes)
 	}
 	nodes->head_tubes = NULL;
 }
-
-void		ft_nodeprint(t_nodes *nodes)
-{
-	t_data	*cur;
-	size_t	len;
-	t_list	*chill;
-	t_data	*thi;
-
-	cur = nodes->head;
-	len = nodes->size;
-	while (cur && len-- > 0)
-	{
-		ft_printf("Room name: %s\n", cur->name);
-		ft_printf("coord X : %d | ", cur->x);
-		ft_printf("coord Y : %d\n", cur->y);
-		ft_printf("Ret BFS: %d\n ", cur->level);
-		ft_printf("Open: %d\n", cur->open);
-		ft_printf("Open: %c\n", cur->role);
-		ft_printf("stat curr : %c\n", cur->statut);
-		chill = cur->chill;
-		while (chill)
-		{
-			thi = ((t_data *)chill->content);
-			ft_printf("Room chill name: %s\n", thi->name);
-			ft_printf("stat chill : %c\n", thi->statut);
-			chill = chill->next;
-		}
-		cur = cur->next;
-	}
-}
