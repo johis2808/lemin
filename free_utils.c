fa60d61fb18c351b2cf276a21dfff7ee877ab65b
joh <totis2808@hotmail.com>
2019-09-26 02:50:45 +0200

    ajout free

--- free_utils.c
+++ free_utils.c
@@ -14,20 +14,14 @@
 
 void	ft_lstfree(t_list **lst)
 {
-	t_list	*list;
 	t_list	*tmp;
 
-	if (!lst)
+	if (!lst || !*lst)
 		return ;
-	list = *lst;
-//	ft_printf("LAAAAA");
-	while (list)
+	while (*lst)
 	{
-	//	ft_printf("LAAAAA");
-		tmp = list;
-		list = list->next;
-	//	if (tmp->content)
-			//free(tmp->content);
+		tmp = *lst;
+		*lst = (*lst)->next;
 		free(tmp);
 	}
 	*lst = NULL;
@@ -39,49 +33,38 @@ void	ft_freelinks(t_chill *chill, size_t size_tubes)
 	size_t	size;
 
 	size = 0;
-//	ft_printf("LAAAAA");
+	if (!chill)
+		return ;
 	while (size < size_tubes && chill)
 	{
-		//ft_printf("LAAAAA");
 		curr = chill;
 		chill = chill->next;
-		if (curr->name)
-			ft_strdel(&curr->name);
-		if (curr->dest)
-			ft_strdel(&curr->dest);
-		free(curr->prev);
 		free(curr);
 		size++;
 	}
-//	chill->next =  NULL;
-//	chill->prev = NULL;
+	chill->next = NULL;
+	chill->prev = NULL;
 }
 
-void	ft_freenodes(t_data *head, size_t size_nodes)
+void	ft_freenodes(t_nodes *nodes)
 {
 	t_data	*curr;
+	t_data	*to_be_free;
 	size_t	size;
 
 	size = 0;
-//	ft_printf("LAAAAA");
-	while (size < size_nodes && head)
+	if(!nodes)
+		return ;
+	curr = nodes->head;
+	while (size < nodes->size)
 	{
-		//ft_printf("LAAAAA");
-		curr = head;
-		if (curr && curr->out)
-			ft_lstfree(&curr->out);
-		if (curr && curr->chill)
-			ft_lstfree(&curr->chill);
-		if (curr->name)
-//			ft_strdel(&curr->name);
-	//	free(curr->prev);
-		head = head->next;
-		if (curr)
-		free(curr);
+		to_be_free = curr;
+		curr = curr->next;
+		free(to_be_free);
 		size++;
 	}
-	//head->next =  NULL;
-	//head->prev = NULL;
+	nodes->head->next = NULL;
+	nodes->head->prev = NULL;
 }
 
 void	ft_freepath(t_path *paths, size_t path_size)
@@ -90,43 +73,61 @@ void	ft_freepath(t_path *paths, size_t path_size)
 	size_t	size;
 
 	size = 0;
+	if (!paths)
+		return ;
 	while (size < path_size && paths)
 	{
-		ft_printf("LAAAAA");
 		curr = paths;
 		paths = paths->next;
 		if (curr->path && curr->path->head)
-			ft_freenodes(curr->path->head, curr->path->size);
-		if (curr->path && curr->path->head_tubes)
-			ft_freelinks(curr->path->head_tubes,curr->path->size_tubes);
-	//	free(curr->prev);
-		else if(curr->path)
-			free(curr);
+			ft_freenodes(curr->path);
+		ft_memdel((void **)&curr->path);
+		free(curr);
 		size++;
 	}
+	paths->next = NULL;
+	paths->prev = NULL;
+}
+
+void	clearcpy(t_nodes *tmpnodes)
+{
+	t_data	*tmp;
+	size_t	i;
+
+	i = 0;
+	if (!tmpnodes)
+		return ;
+	tmp = tmpnodes->head;
+	while (i < tmpnodes->size)
+	{	
+		ft_lstfree(&tmp->chill);
+		tmp = tmp->next;
+		i++;
+	}
+	ft_freenodes(tmpnodes);
+	ft_memdel((void **)&tmpnodes);
 }
 
 void	ft_close_lemin(t_param *params)
 {
-//	ft_printf("LAAAAA");
-	if (params->nodes->head)
-		ft_freenodes(params->nodes->head, params->nodes->size);
-	//free(params->nodes->head);
-	params->nodes->head = NULL;
-	if (params->aya)
-		ft_freenodes(params->aya, params->nb_paths);
-	//free(params->aya);
-	params->aya = NULL;
-	if (params->tmpnodes->head)
-		ft_freenodes(params->tmpnodes->head, params->tmpnodes->size);
-	//free(params->tmpnodes->head);
-	params->tmpnodes->head = NULL;
+	t_data	*tmp;
+	size_t	i;
+
+	i = 0;
+	tmp = params->nodes->head;
+	while (i < params->nodes->size)
+	{	
+		ft_lstfree(&tmp->chill);
+		ft_memdel((void **)&tmp->name);
+		tmp = tmp->next;
+		i++;
+	}
+	ft_freenodes(params->nodes);
+	ft_freelinks(params->nodes->head_tubes, params->nodes->size_tubes);
+	ft_memdel((void **)&params->nodes);
+	clearcpy(params->tmpnodes);
 	ft_lstfree(&params->queu);
-	ft_freepath(params->paths->head,params->paths->nb_path);
-	//free(params->paths);
-	params->paths= NULL;
-	ft_freepath(params->old_paths->head,params->old_paths->nb_path);
-	//free(params->old_paths);
-	params->old_paths = NULL;
-	params->queu = NULL;
+	ft_freepath(params->paths->head, params->paths->nb_path);
+	ft_memdel((void **)&params->paths);
+	ft_memdel((void **)&params);
 }
